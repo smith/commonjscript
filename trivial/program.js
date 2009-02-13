e515dbe2a00a9b12fd6de80dcdd86f785564103c
cowbertvonmoo <cowbertvonmoo@6cc3cf6a-f3ef-11dd-afc9-ff9d66c6243e>
2009-02-13 02:06:55 +0000

    Revised the trivial example and added an absolute identifier test.

--- trivial/program.js
+++ trivial/program.js
@@ -2,7 +2,14 @@
 // program.js
 //
 
+var print;
+if (environment.print) {
+    print = environment.print;
+} else if (typeof alert !== "undefined") {
+    print = alert;
+}
+
 var inc = require('increment').increment;
 var a = 1;
-alert("inc(" + a + ") = " + inc(a))
+print("inc(" + a + ") = " + inc(a))
 
